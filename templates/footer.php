83c9b6ae0226ceac912b25e8826181e04bbc784f
Micheal Bailey <mbaileyfuturist@gmail.com>
2020-08-30 17:42:45 -0400

    connected contact form to sql database.

--- templates/footer.php
+++ templates/footer.php
@@ -68,18 +68,60 @@
         </footer>
     </body>
 
+    <!-- Scroll Magic -->
     <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js"></script>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js"></script>
+        
+        <!-- Particles.js -->
         <script src="https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js"></script>
         <script>
             particlesJS.load('particles-js','../static/particles.json', function(){
                 console.log('particles.jason loaded...');
             })
         </script>
-    <script src="../static/sidenav.js"></script>
+
+    <!-- Ion Icons -->
     <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap -->
+    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     
+     <!-- In House JS -->
+    <script src="../static/sidenav.js"></script>
+
+    <script>
+        function submitContactForm(){
+
+            //Use JQuery to grab the values from from the form by their ID, these values are in the header.
+            var first_name = $('#inputFirstName').val();
+            var last_name = $('#inputLastName').val();
+            var email = $('#inputEmail').val();
+            var company_name = $('#inputCompanyName').val();
+            var client_message = $('#inputMessage').val();
+
+            //Use AJAX to post this data.
+            $.ajax({
+                type:'POST',
+                url:'header.php',
+                data:'contactFrmSubmit=1&first_name='+first_name+'&last_name='+last_name+'&email='+email+'&company_name='+company_name+'&client_message='+client_message,
+                success:function(msg){
+                        
+                        //Clear the input values.
+                        $('#inputFirstName').val('');
+                        $('#inputLastName').val('');
+                        $('#inputEmail').val('');
+                        $('#inputCompanyName').val('');
+                        $('#inputMessage').val('');
+
+                        //View the message.
+                        console.log(msg);
+                },
+                error:function(){
+                    console.log("An error has occured.");
+                }
+            });
+        }
+        </script>
 </html>
--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,33 @@
+<?php 
+        //Connect to the database.
+        include('../config/db_connect.php');
+        
+        //If form has been submitted.
+        if(isset($_POST['contactFrmSubmit'])){
+
+            //Protection from SQLInjection.
+            $first_name = mysqli_real_escape_string($conn, $_POST['first_name']);
+            $last_name = mysqli_real_escape_string($conn, $_POST['last_name']);
+            $email = mysqli_real_escape_string($conn, $_POST['email']);
+            $company_name = mysqli_real_escape_string($conn, $_POST['company_name']);
+            $client_message = mysqli_real_escape_string($conn, $_POST['client_message']);        
+
+            //sql insert
+            $sql = "INSERT INTO potential_clients(first_name, last_name, email, company_name, client_message) VALUES('$first_name', '$last_name', '$email', '$company_name', '$client_message')";
+
+            //Save to db and check.
+            if(mysqli_query($conn, $sql)){
+                echo "Insert Success!";
+            }else{
+                echo "query error ".mysqli_error($conn);
+            }
+
+        }
+?>
+
 <!DOCTYPE html>
 
-<html class="no-js">
+<html>
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,8 +35,13 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="../static/style.css?v=1.1">
+
+        <!-- Bootstrap CDN -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        
+        <!-- Google Fonts API -->
         <link href="https://fonts.googleapis.com/css2?family=Recursive&display=swap" rel="stylesheet">
+
     </head>
     <body>
 
@@ -57,26 +89,26 @@
                     </button>
                     </div>
                     <div class="modal-body">
-                        <form action="/action_page.php">
+                        <form action="header.php" method="POST">
                             <div class="row">
-                                <input type="text" class="ml-3 mr-1 form-control col" placeholder="first name" id="first_name">
-                                <input type="text" class=" mr-3 ml-1 form-control col" placeholder="last name" id="last _name">
+                                <input type="text" class="ml-3 mr-1 form-control col" placeholder="first name" id="inputFirstName" name="first_name">
+                                <input type="text" class=" mr-3 ml-1 form-control col" placeholder="last name" id="inputLastName" name="last_name">
                             </div>
                             
                             <div class="form-group mt-2">
-                                <input type="email" class="form-control" placeholder="example@email.com" id="email">
+                                <input type="email" class="form-control" placeholder="example@email.com" id="inputEmail" name="email">
                             </div>
 
                             <div class="form-group mt-2">
-                                <input type="text" class="form-control" placeholder="company name" id="company_name">
+                                <input type="text" class="form-control" placeholder="company name" id="inputCompanyName" name="company_name">
                             </div>
 
-                            <divn class="form-group mt-2">
-                                <textarea class="form-control" aria-label="With textarea"></textarea>
+                            <div class="form-group mt-2">
+                                <textarea class="form-control" id="inputMessage" name="client_message" placeholder="message here..."></textarea>
                             </div>
 
                             <div class="d-flex justify-content-center mb-3">
-                                <button type="button" class="btn btn-primary submit-btn" data-dismiss="modal">submit</button>
+                                <button type="submit" name="submit" class="btn btn-primary submitBtn" data-dismiss="modal" onClick="submitContactForm()">submit</button>
                             </div>
                         </form>
                     </div>  
